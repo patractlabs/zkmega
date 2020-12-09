6f3e562dd82462c8485729816dc31cb385e99177
clearloop <udtrokia@gmail.com>
2020-12-09 16:04:16 +0800

    refactor(groth16): move groth16 verify to arkworks

---
+++ crates/arkworks/src/curve.rs
@@ -0,0 +1,6 @@
+//! Re-export curves
+pub use ark_bls12_377::Bls12_377;
+pub use ark_bls12_381::Bls12_381;
+pub use ark_bn254::Bn254;
+pub use ark_bw6_761::BW6_761;
+pub use ark_cp6_782::CP6_782;
--- crates/arkworks/src/lib.rs
+++ crates/arkworks/src/lib.rs
@@ -6,11 +6,8 @@
 #![no_std]
 mod derive;
 
-// mod bls12_377;
-// mod bls12_381;
-// mod bn254;
-// mod bw6_761;
-// mod cp6_782;
+pub mod curve;
+pub mod groth16;
 pub mod ops;
 pub mod tests;
 
@@ -26,14 +23,15 @@ pub fn add(curve_id: i32, input: &[u8]) -> Result<Vec<u8>, SerializationError> {
         0x2a => <ark_bls12_377::Bls12_377 as CurveBasicOperations>::add(input),
         0x2b => <ark_bls12_381::Bls12_381 as CurveBasicOperations>::add(input),
         0x2c => <ark_bn254::Bn254 as CurveBasicOperations>::add(input),
-        0x2d => <ark_cp6_782::CP6_782 as CurveBasicOperations>::add(input),
-        _ => Err(Error::new(ErrorKind::Other, "Invalid curve id"))?,
+        0x2d => <ark_bw6_761::BW6_761 as CurveBasicOperations>::add(input),
+        _ => Err(Error::new(ErrorKind::Other, "Invalid curve id").into()),
     }
 }
 
+/// Vector Addition
 #[cfg(feature = "ink")]
 pub fn add(input: &[u8]) -> Result<bool, SerializationError> {
-    ink_env::zk_snarks::add(CURVE_ID, input)
+    ink_env::pairing::add(CURVE_ID, input)
 }
 
 /// Scalar MulAssign
@@ -43,14 +41,15 @@ pub fn mul(curve_id: i32, input: &[u8]) -> Result<Vec<u8>, SerializationError> {
         0x2a => <ark_bls12_377::Bls12_377 as CurveBasicOperations>::mul(input),
         0x2b => <ark_bls12_381::Bls12_381 as CurveBasicOperations>::mul(input),
         0x2c => <ark_bn254::Bn254 as CurveBasicOperations>::mul(input),
-        0x2d => <ark_cp6_782::CP6_782 as CurveBasicOperations>::mul(input),
-        _ => Err(Error::new(ErrorKind::Other, "Invalid curve id"))?,
+        0x2d => <ark_bw6_761::BW6_761 as CurveBasicOperations>::mul(input),
+        _ => Err(Error::new(ErrorKind::Other, "Invalid curve id").into()),
     }
 }
 
+/// Scalar MulAssign
 #[cfg(feature = "ink")]
 pub fn mul(input: &[u8]) -> Result<bool, SerializationError> {
-    ink_env::zk_snarks::add(CURVE_ID, input)
+    ink_env::pairing::add(CURVE_ID, input)
 }
 
 /// Pairing
@@ -60,21 +59,31 @@ pub fn pairing(curve_id: i32, input: &[u8]) -> Result<Vec<u8>, SerializationErro
         0x2a => <ark_bls12_377::Bls12_377 as CurveBasicOperations>::mul(input),
         0x2b => <ark_bls12_381::Bls12_381 as CurveBasicOperations>::mul(input),
         0x2c => <ark_bn254::Bn254 as CurveBasicOperations>::mul(input),
-        0x2d => <ark_cp6_782::CP6_782 as CurveBasicOperations>::mul(input),
-        _ => Err(Error::new(ErrorKind::Other, "Invalid curve id"))?,
+        0x2d => <ark_bw6_761::BW6_761 as CurveBasicOperations>::mul(input),
+        _ => Err(Error::new(ErrorKind::Other, "Invalid curve id").into()),
     }
 }
 
+/// Pairing
 #[cfg(feature = "ink")]
 pub fn pairing(input: &[u8]) -> Result<bool, SerializationError> {
-    ink_env::zk_snarks::add(CURVE_ID, input)
+    ink_env::pairing::add(CURVE_ID, input)
 }
 
-/// Re-export curves
-pub mod curve {
-    pub use ark_bls12_377::Bls12_377;
-    pub use ark_bls12_381::Bls12_381;
-    pub use ark_bn254::Bn254;
-    pub use ark_bw6_761::BW6_761;
-    pub use ark_cp6_782::CP6_782;
+/// Groth16 Verifaction
+#[cfg(not(feature = "ink"))]
+pub fn groth16_verify(
+    curve_id: i32,
+    vk_gamma_abc: &[&[u8]],
+    vk: &[u8],
+    proof: &[u8],
+    public_inputs: &[&[u8]],
+) -> Result<bool, SerializationError> {
+    match curve_id {
+        0x2a => groth16::verify::<ark_bls12_377::Bls12_377>(vk_gamma_abc, vk, proof, public_inputs),
+        0x2b => groth16::verify::<ark_bls12_381::Bls12_381>(vk_gamma_abc, vk, proof, public_inputs),
+        0x2c => groth16::verify::<ark_bn254::Bn254>(vk_gamma_abc, vk, proof, public_inputs),
+        0x2d => groth16::verify::<ark_bw6_761::BW6_761>(vk_gamma_abc, vk, proof, public_inputs),
+        _ => Err(Error::new(ErrorKind::Other, "Invalid curve id").into()),
+    }
 }
