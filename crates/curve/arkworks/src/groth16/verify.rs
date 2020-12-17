7ab1cf64430a597374a6ad4a5399d6d930379f47
clearloop <udtrokia@gmail.com>
2020-12-17 15:53:50 +0800

    refactor(crates/curve): udpate function id

--- crates/curve/arkworks/src/groth16/verify.rs
+++ crates/curve/arkworks/src/groth16/verify.rs
@@ -33,13 +33,13 @@ pub fn verify<C: CurveBasicOperations>(
         mul_input[0..g1_len].copy_from_slice(b);
         mul_input[g1_len..g1_len + scalar_len].copy_from_slice(i);
 
-        let mul_ic = crate::call(C::CURVE_ID + 0x3a, &mul_input)?;
+        let mul_ic = crate::call(0x01000001 + C::CURVE_ID, &mul_input)?;
 
         let mut acc_mul_ic = Vec::with_capacity(g1_len * 2);
         acc_mul_ic[0..g1_len].copy_from_slice(acc.as_ref());
         acc_mul_ic[g1_len..g1_len * 2].copy_from_slice(mul_ic.as_ref());
 
-        acc = crate::call(C::CURVE_ID + 0x2a, &*acc_mul_ic)?;
+        acc = crate::call(0x01000000 + C::CURVE_ID, &*acc_mul_ic)?;
     }
 
     // The original verification equation is:
@@ -86,7 +86,7 @@ pub fn verify<C: CurveBasicOperations>(
     // Return the result of computing the pairing check
     // e(p1[0], p2[0]) *  .... * e(p1[n], p2[n]) == 1.
     // For example pairing([P1(), P1().negate()], [P2(), P2()]) should return true.
-    Ok(crate::call(C::CURVE_ID + 0x4a, &input)?[0] == 0)
+    Ok(crate::call(0x01000002 + C::CURVE_ID, &input)?[0] == 0)
 }
 
 fn negate_y_based_curve(y: BigUint, prime_field: &'static str) -> Result<BigUint> {
